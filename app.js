80bba59b32bfe15399952c274b7a1a7135dc6b6f
HrachCode <virap1000@mail.ru>
2020-02-11 22:18:29 +0400

    olla

--- app.js
+++
@@ -1,9 +0,0 @@
-const express  = require('express')
-const app = express();
-
-app.get('*',(req,res)=>{
-    res.send('<h1>hello worold</h1>')
-})
-
-app.listen(8080,console.log('listening port 8080')
-)
